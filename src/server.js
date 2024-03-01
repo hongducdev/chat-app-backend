560cd64011b4e786173365339d2d6eceb50386cb
d4Jt <jsmsj04@gmail.com>
2024-03-01 22:27:50 +0700

    Update all code

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import userRoutes from './routes/user.route.js';
+import { app, server } from './socket/socket.js';
+
+const PORT = process.env.PORT || 4090;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+server.listen(PORT, () => {
+   connectToMongoDB();
+   console.log(`Server Running on port ${PORT}`);
+});
