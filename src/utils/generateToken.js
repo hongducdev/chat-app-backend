18d2be4976af70085dc09e4137bce85b3bb1ed40
d4Jt <jsmsj04@gmail.com>
2024-03-01 23:38:59 +0700

    Update CORS

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = async (userId, res) => {
    res.cookie('jwt', token, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       secure: process.env.NODE_ENV != 'development',
    });
 };
