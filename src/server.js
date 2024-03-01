e634c1c1898da9b653a5fbabfdbe837e4143fde8
d4Jt <jsmsj04@gmail.com>
2024-03-02 00:03:04 +0700

    Update CORS

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use(
    cors({
       origin: ['http://localhost:4953'],
+      credentials: true,
    })
 );
 
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -9,6 +9,7 @@ const io = new Server(server, {
    cors: {
       origin: ['http://localhost:4953'],
       methods: ['GET', 'POST'],
+      credentials: true,
    },
 });
 
