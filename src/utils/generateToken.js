9c1bbda38fac69da19acef347859bd60329b2f0d
d4Jt <jsmsj04@gmail.com>
2024-03-02 00:44:19 +0700

    Fix cookie

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = async (userId, res) => {
-   return (token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+const generateToken= async (userId, res) => {
+   return jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
       expiresIn: '30d',
-   }));
+   });
 };
 
 export default generateToken;
