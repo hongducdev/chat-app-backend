ec8c23147da73fd7b52fa82286b5a2fb8332a403
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2024-03-02 19:41:15 +0700

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
       res.cookie('jwt', token, {
          maxAge: 30 * 24 * 60 * 60 * 1000,
          httpOnly: true,
-         sameSite: 'none',
+         sameSite: 'lax',
          secure: false,
       });
 
