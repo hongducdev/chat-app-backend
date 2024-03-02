86d091bd6e3ad31e472db13f4df03ee556bd3875
d4Jt <jsmsj04@gmail.com>
2024-03-02 15:39:00 +0700

    Swagger docs API

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import userRoutes from './routes/user.route.js';
+import testRoutes from './tests/index.route.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 4090;
@@ -24,11 +25,13 @@ app.use(
 );
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger-output.json' with { type: "json" };
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+app.use('/test', testRoutes)
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.listen(PORT, () => {
    connectToMongoDB();
--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./src/server.js'];
+const routes = ['./src/tests/index.route.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
